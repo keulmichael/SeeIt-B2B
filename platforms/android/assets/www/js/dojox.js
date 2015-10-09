f440da2ce39b5a27f3e03956776d1ca977f6a33d
Host <host.michael@gmail.com>
2015-10-09 10:12:08 +0200

    Update dojox.js

--- platforms/android/assets/www/js/dojox.js
+++ platforms/android/assets/www/js/dojox.js
@@ -431,8 +431,8 @@ var Dynamisme = document.getElementById("InputDynamisme").value;
 var Gilet = document.getElementById("InputGilet").value;
 var Spots = document.getElementById("InputSpots").value;
 var Ecrans = document.getElementById("InputEcrans").value;
-var PLV = document.getElementById("InputPLV").value;
-request.get('http://www.appliseeit.com/mobile/envoiEmailRapportBtoB.php?magasinControle='+magasinControle+'&FacadeVitrine='+FacadeVitrine+'&Surfacevente='+Surfacevente+'&Stock='+Stock+'&Produits='+Produits+'&ModeleExpo='+ModeleExpo+'&FichesEtPrix='+FichesEtPrix+'&PrixCorrectes='+PrixCorrectes+'&Visibilite='+Visibilite+'&Equipe='+Equipe+'&Dynamisme='+Dynamisme+'&Gilet='+Gilet+'&Spots='+Spots+'&Ecrans='+Ecrans+'&PLV='+PLV).then(
+var PLVV = document.getElementById("InputPLV").value;
+request.get('http://www.appliseeit.com/mobile/envoiEmailRapportBtoB.php?magasinControle='+magasinControle+'&FacadeVitrine='+FacadeVitrine+'&Surfacevente='+Surfacevente+'&Stock='+Stock+'&Produits='+Produits+'&ModeleExpo='+ModeleExpo+'&FichesEtPrix='+FichesEtPrix+'&PrixCorrectes='+PrixCorrectes+'&Visibilite='+Visibilite+'&Equipe='+Equipe+'&Dynamisme='+Dynamisme+'&Gilet='+Gilet+'&Spots='+Spots+'&Ecrans='+Ecrans+'&PLV='+PLVV).then(
 function(responseEnvoiEmailRapport){document.getElementById("afficherConfirmationEnvoiRapportBtoB").innerHTML=responseEnvoiEmailRapport;}
 )
 })
